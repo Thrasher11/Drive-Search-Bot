16383315a33b3320b8ef76c8edcd14c93d4b802e
Thrasher11 <71846792+Thrasher11@users.noreply.github.com>
2021-09-08 23:04:30 +0530

    Add files via upload

--- telegraph_token.py
+++ telegraph_token.py
@@ -1,6 +1,6 @@
-from telegraph import Telegraph
+﻿from telegraph import Telegraph
 
 telegraph = Telegraph()
-telegraph.create_account(short_name=input("Enter a username for your Telegra.ph : "))
+telegraph.create_account(short_name=input("Enter a username for your Telegra.ph : Bumble_beeeeee"))
 
 print(f"Your Telegra.ph token ==>  {telegraph.get_access_token()}")
